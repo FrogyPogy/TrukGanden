fa41ac50184b5516608f15882884b45160af290d
FrogyPogy <100332786+FrogyPogy@users.noreply.github.com>
2022-05-24 23:30:40 +0700

    Add files via upload

---
+++ TrukGandeng/jalan.h
@@ -0,0 +1,14 @@
+// Tanggal : Senin, 23 Mei 2022
+// Lab : GKV A2
+
+#ifndef jalan_hpp
+#define jalan_hpp
+
+#include <stdio.h>
+class Jalan {
+public:
+    void jalan();
+    void pinggiranJalan();
+};
+
+#endif /* jalan_hpp */
